30aeeca68f8528a2e8edfedc4cdd931b09453edd
mustafa mohammmed <mustafaastal99@gmail.com>
2020-09-09 12:34:57 +0300

    toolbar

--- app/src/main/java/com/example/quranproject/Log_in_Center.java
+++ app/src/main/java/com/example/quranproject/Log_in_Center.java
@@ -4,9 +4,12 @@ import android.os.Bundle;
 import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 public class Log_in_Center extends AppCompatActivity {
 Button test;
+
+Toolbar toolbar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
