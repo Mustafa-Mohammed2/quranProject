a8417143aaac8fcf2402a559d41bef988110ff7a
Ahmed-Redwan <MINGW64>
2020-09-09 12:10:32 +0300

    add btn

--- app/src/main/java/com/example/quranproject/Log_in_Center.java
+++ app/src/main/java/com/example/quranproject/Log_in_Center.java
@@ -1,11 +1,12 @@
 package com.example.quranproject;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class Log_in_Center extends AppCompatActivity {
-
+Button test;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
