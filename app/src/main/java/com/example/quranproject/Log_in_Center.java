92e2c02d9aaee65d8138e7bb60fe8ae8b07f8d13
Ahmed-Redwan <MINGW64>
2020-09-09 12:22:01 +0300

    add btn

--- app/src/main/java/com/example/quranproject/Log_in_Center.java
+++ app/src/main/java/com/example/quranproject/Log_in_Center.java
@@ -11,5 +11,6 @@ Button test;
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_log_in__center);
+
     }
 }
