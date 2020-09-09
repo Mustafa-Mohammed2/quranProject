b530a2e49c325439b5af87dedf446e0237ffc816
mustafa mohammmed <mustafaastal99@gmail.com>
2020-09-09 12:13:52 +0300

    tool bar

--- app/src/main/java/com/example/quranproject/Log_in_Center.java
+++ app/src/main/java/com/example/quranproject/Log_in_Center.java
@@ -4,16 +4,18 @@ import android.os.Bundle;
 import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 public class Log_in_Center extends AppCompatActivity {
 
     Button btn ;
+    Toolbar toolbar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_log_in__center);
 
         btn=findViewById(R.id.log_center_btn_log);
-
+        toolbar=findViewById(R.id.log_center_tool);
     }
 }
