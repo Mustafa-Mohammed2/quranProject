6336b521875306d4885cbe19cfe1a21849a839ce
mustafa mohammmed <mustafaastal99@gmail.com>
2020-09-09 11:38:05 +0300

    mustafa mustafa mustafa

--- app/src/main/java/com/example/quranproject/Log_in_Center.java
+++ app/src/main/java/com/example/quranproject/Log_in_Center.java
@@ -1,14 +1,19 @@
 package com.example.quranproject;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class Log_in_Center extends AppCompatActivity {
 
+    Button btn ;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_log_in__center);
+
+        btn=findViewById(R.id.log_center_btn_log);
+
     }
 }
